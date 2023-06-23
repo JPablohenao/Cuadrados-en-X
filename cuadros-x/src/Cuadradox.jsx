07fb5d75825760b732b43c9efc6c65694f0a868e
Pablo <106776147+JPablohenao@users.noreply.github.com>
2023-06-22 19:35:58 -0500

    dentacion

--- cuadros-x/src/Cuadradox.jsx
+++ cuadros-x/src/Cuadradox.jsx
@@ -1,38 +1,38 @@
 import React from "react";
 
-function CuadrodadoX () {
+function CuadrodadoX() {
   const dibujarCuadrado = (numero) => {
     if (numero < 5 || numero > 10) {
-   alert("El numero debe ser entre  5 y 10 ");
-    return;
+      alert("El numero debe ser entre  5 y 10 ");
+      return;
     }
-  let cuadrado = "";
-  for (let fila = 0; fila < numero; fila++){
-    let filaCuadrado = "";
-    for (let columna = 0; columna < numero; columna++){
-    if (
-      fila === 0 ||
-      fila === numero - 1 ||
-      columna === 0 ||
-      columna === numero -1 ||
-      fila === columna
-    ) {
-      filaCuadrado +="x";
-  } else {
-      filaCuadrado +="-";
-   } 
-  }
-cuadrado += filaCuadrado +"\n";
-}
-alert(cuadrado);
-};
+    let cuadrado = "";
+    for (let fila = 0; fila < numero; fila++) {
+      let filaCuadrado = "";
+      for (let columna = 0; columna < numero; columna++) {
+        if (
+          fila === 0 ||
+          fila === numero - 1 ||
+          columna === 0 ||
+          columna === numero - 1 ||
+          fila === columna
+        ) {
+          filaCuadrado += "x";
+        } else {
+          filaCuadrado += "-";
+        }
+      }
+      cuadrado += filaCuadrado + "\n";
+    }
+    alert(cuadrado);
+  };
 
-return (
-  <diV>
-    <h1> Cuadros  en X </h1> 
-    <button onClick={() => dibujarCuadrado(8)}> click para dibujar Cuadrado</button>
-  </diV>
-);
-} 
+  return (
+    <diV>
+      <h1> Cuadros  en X </h1>
+      <button onClick={() => dibujarCuadrado(8)}> click para dibujar Cuadrado</button>
+    </diV>
+  );
+}
 
 export default CuadrodadoX; 
